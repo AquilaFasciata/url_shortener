19c9610b2cef88529f52dba127d73ffa3009f426
Zach Saunders <zachisaunders12@gmail.com>
2024-09-02 12:55:53 -0400

    Added db init script for future docker use

---
+++ dbinit.sql
@@ -0,0 +1,21 @@
+CREATE TABLE "users"(
+    "id" BIGINT NOT NULL,
+    "username" TEXT NOT NULL,
+    "hashed_pw" TEXT NOT NULL,
+    "email" TEXT NOT NULL
+);
+ALTER TABLE
+    "users" ADD PRIMARY KEY("id");
+CREATE TABLE "urls"(
+    "id" BIGINT NOT NULL,
+    "shorturl" TEXT NOT NULL,
+    "longurl" TEXT NOT NULL,
+    "created_by" BIGINT NULL,
+    "clicks" BIGINT NOT NULL
+);
+CREATE INDEX "urls_shorturl_index" ON
+    "urls"("shorturl");
+ALTER TABLE
+    "urls" ADD PRIMARY KEY("id");
+ALTER TABLE
+    "urls" ADD CONSTRAINT "urls_created_by_foreign" FOREIGN KEY("created_by") REFERENCES "users"("id");
