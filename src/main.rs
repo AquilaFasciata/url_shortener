bcaab1436576c719a1db77a8c9238d633fe6c710
Zach <zachisaunders12@gmail.com>
2024-09-03 19:17:32 -0400

    Minor minor progress

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router, Html};
+use tokio::fs;
+
+#[tokio::main]
+async fn main() {
+    let router = Router::new();
+
+    let app = router.route("/", get(root()));
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn root() -> Html< {
+    let contents = fs::read("html/index.html");
+    return Html(contents)
 }
