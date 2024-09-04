aa0b87fe363fe284bf5b27d9ccac023cfc46ed9d
Zach Saunders <zachisaunders12@gmail.com>
2024-09-03 23:56:27 -0400

    Started building out server component

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,22 @@
-use axum::{routing::get, Router, Html};
+use axum::{
+    http::HeaderMap,
+    response::{Html, IntoResponse, Response},
+    routing::get,
+    Router,
+};
 use tokio::fs;
 
 #[tokio::main]
 async fn main() {
     let router = Router::new();
 
-    let app = router.route("/", get(root()));
+    let app = router.route("/", get(root));
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn root() -> Html< {
-    let contents = fs::read("html/index.html");
-    return Html(contents)
+async fn root() -> Response {
+    let contents = fs::read("html/index.html").await.unwrap();
+    let html = Html::from(contents);
+    return html.into_response();
 }
