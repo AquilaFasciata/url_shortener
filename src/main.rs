9bb9c0feb70d230ed31d3c297c729d3134d1f20d
Zach Saunders <zachisaunders12@gmail.com>
2024-09-05 23:30:46 -0400

    Added some logic to routing

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 use axum::{
+    body::Body,
     extract::Path,
-    http::{HeaderMap, StatusCode},
-    response::{Html, IntoResponse, Response},
+    http::{
+        header::{self, HeaderValue},
+        StatusCode,
+    },
+    response::{Html, IntoResponse, Redirect, Response},
     routing::get,
     Router,
 };
-use tokio::fs;
 use regex::Regex;
+use tokio::fs;
 
 #[tokio::main]
 async fn main() {
@@ -25,20 +29,35 @@ async fn root() -> Response {
     return html.into_response();
 }
 
+#[forbid(unsafe_code)]
 async fn derivative(Path(extra): Path<String>) -> Response {
     // TODO Seperate Html and CSS responses
     let mut path = String::from("html/");
-    let mut extra = extra;
-    if extra.starts_with("..") {
-        extra.drain(0..2);
+    if extra.contains("..") {
+        return StatusCode::FORBIDDEN.into_response();
     }
     println!("Retriving file {}", &extra);
     path.push_str(extra.as_str());
-    let contents = match fs::read(path).await {
+    let contents = match fs::read(&path).await {
         Ok(content) => content,
         Err(_) => return StatusCode::NOT_FOUND.into_response(),
     };
-    let file_ext_regex = Regex::new(r"\.\w+$");
-    let html = Html::from(contents);
-    return html.into_response();
+
+    let file_ext_regex: Regex = Regex::new(r"\.\w+$").expect("Error creating Regex match");
+    let file_ext = match file_ext_regex.find(path.as_str()) {
+        Some(strtype) => strtype,
+        None => return StatusCode::NOT_FOUND.into_response(),
+    };
+    match file_ext.as_str() {
+        ".html" => Html::from(contents).into_response(),
+        ".css" => return css_response_builder(contents),
+        _ => return Redirect::to("/index.html").into_response(),
+    }
+}
+
+fn css_response_builder(contents: Vec<u8>) -> Response {
+    let mut resp = Body::from(contents).into_response();
+    resp.headers_mut()
+        .insert(header::CONTENT_TYPE, HeaderValue::from_static("text/css"));
+    return resp;
 }
