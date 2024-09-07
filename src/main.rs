42b10978d95782606073c63eea235f4aa872bc82
Zach Saunders <zachisaunders12@gmail.com>
2024-09-06 22:37:14 -0400

    Updated some html content; implemented functions for 404 and some updates accounting for that

--- src/main.rs
+++ src/main.rs
@@ -40,7 +40,7 @@ async fn derivative(Path(extra): Path<String>) -> Response {
     path.push_str(extra.as_str());
     let contents = match fs::read(&path).await {
         Ok(content) => content,
-        Err(_) => return StatusCode::NOT_FOUND.into_response(),
+        Err(_) => return not_found_handler().await,
     };
 
     let file_ext_regex: Regex = Regex::new(r"\.\w+$").expect("Error creating Regex match");
@@ -50,14 +50,22 @@ async fn derivative(Path(extra): Path<String>) -> Response {
     };
     match file_ext.as_str() {
         ".html" => Html::from(contents).into_response(),
-        ".css" => return css_response_builder(contents),
-        _ => return Redirect::to("/index.html").into_response(),
+        ".css" => return content_response(contents, HeaderValue::from_static("text/css")),
+        _ => return not_found_handler().await,
     }
 }
 
-fn css_response_builder(contents: Vec<u8>) -> Response {
+fn content_response(contents: Vec<u8>, content_type: HeaderValue) -> Response {
     let mut resp = Body::from(contents).into_response();
     resp.headers_mut()
-        .insert(header::CONTENT_TYPE, HeaderValue::from_static("text/css"));
+        .insert(header::CONTENT_TYPE, content_type);
     return resp;
+}
+
+async fn not_found_handler() -> Response {
+    let content = fs::read("html/404.html").await.unwrap();
+    Response::builder()
+        .status(StatusCode::NOT_FOUND)
+        .body(Body::from(content))
+        .expect("Failed to build 404 response")
 }
