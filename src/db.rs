25fdbaba055536b040711a12d9abbc30dead1dae
Zach Saunders <zachisaunders12@gmail.com>
2024-09-16 23:22:00 -0400

    Added some structs and create_url signature

--- src/db.rs
+++ src/db.rs
@@ -1,5 +1,18 @@
-use sqlx::{Executor, query::Query};
+struct UrlRow {
+    id: i64,
+    shorturl: String,
+    longurl: String,
+    created_by: Option<i64>,
+    clicks: i64
+}
 
-pub fn create_url(long_url: &str, user_id: Option<i64>, connection_pool: sqlx::Pool) -> Result {
-    let query = 
+struct UserRow {
+    id: i64,
+    username: String,
+    hashed_pw: String,
+    email: String
+}
+
+pub fn create_url(long_url: &str, user_id: Option<i64>, connection_pool: sqlx::Pool) -> Result<&str, sqlx::Error> {
+
 }
