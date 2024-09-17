56649e54b75573afb24c253bfacb0f0b2b2a2163
Zach Saunders <zachisaunders12@gmail.com>
2024-09-17 08:11:29 -0400

    Began working on url hashing function

--- src/db.rs
+++ src/db.rs
@@ -1,4 +1,8 @@
-struct UrlRow {
+use sqlx::FromRow;
+use rand::SeedableRng;
+
+#[derive(FromRow)]
+pub struct UrlRow {
     id: i64,
     shorturl: String,
     longurl: String,
@@ -6,7 +10,8 @@ struct UrlRow {
     clicks: i64
 }
 
-struct UserRow {
+#[derive(FromRow)]
+pub struct UserRow {
     id: i64,
     username: String,
     hashed_pw: String,
@@ -14,5 +19,13 @@ struct UserRow {
 }
 
 pub fn create_url(long_url: &str, user_id: Option<i64>, connection_pool: sqlx::Pool) -> Result<&str, sqlx::Error> {
+     
+}
 
+fn gen_url_keyword(long_url: &str) {
+    let alphabet_arr = [
+        ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'],
+        ['k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't'],
+        ['u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd']
+    ];
 }
