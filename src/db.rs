590af752c29b10a5a39b6415f30d0bf318535185
Zach Saunders <zachisaunders12@gmail.com>
2024-09-20 06:02:16 -0400

    Added some functionality

--- src/db.rs
+++ src/db.rs
@@ -1,4 +1,7 @@
+use super::DEFAULT_URL_LEN;
+use core::str;
 use sqlx::FromRow;
+use std::str::from_utf8;
 use uuid::Uuid;
 
 #[derive(FromRow)]
@@ -25,8 +28,10 @@ pub fn create_url(
 ) -> Result<&str, sqlx::Error> {
 }
 
-fn gen_url_keyword(long_url: &str) -> &str {
+fn gen_url_longword(long_url: &str) -> &str {
+    let mut buffer: [u8; 16] = [0; 16];
     let uuid = Uuid::new_v3(&Uuid::NAMESPACE_OID, &long_url.as_bytes());
+    uuid.clone().as_simple().encode_lower(&mut buffer);
+    let buff_str = str::from_utf8(&(buffer.clone())).unwrap();
+    return buff_str;
 }
-
-fn does_keyword_exist(keyword: &str, pool: sqlx::PgPool) -> bool {}
